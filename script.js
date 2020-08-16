6715ee26be9f8d383b4f5da5a7ae678e872f1067
Jakub Chmura <jakub.chmura94@gmail.com>
2020-08-16 16:01:34 +0200

    Remove done tasks by remove button, added

--- script.js
+++ script.js
@@ -10,13 +10,24 @@
         for (const task of tasks) {
             htmlString += `
         <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
+        <button class="js-done">Zrobione</button>
         ${task.content}
         </li>
+        <button class="js-remove">Usuń</button>
         `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
-    }
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+        
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                tasks.splice(index, 1);
+                render();
+            });
+        });
+    };
 
     const init = () => {
         render();
