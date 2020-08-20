b7d511ea1a4a408b3f90e55395ec0388962aa37b
Jakub Chmura <jakub.chmura94@gmail.com>
2020-08-20 13:15:23 +0200

    tasks done added

--- script.js
+++ script.js
@@ -1,30 +1,59 @@
 {
-    const tasks = [ ];
+    const tasks = [];
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    }
+
+    const bindEvents = () => {
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
+            });
+        });
+    };
 
     const render = () => {
         let htmlString = "";
-
 
         for (const task of tasks) {
             htmlString += `
         <li${task.done ? " style=\"text-decoration: line-through\"" : ""}>
         <button class="js-done">Zrobione</button>
         ${task.content}
-        </li>
         <button class="js-remove">Usuń</button>
+        </li>
         `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
 
-        const removeButtons = document.querySelectorAll(".js-remove");
-        
-        removeButtons.forEach((removeButton, index) => {
-            removeButton.addEventListener("click", () => {
-                tasks.splice(index, 1);
-                render();
-            });
-        });
+        bindEvents();
     };
 
     const init = () => {
@@ -41,7 +70,7 @@
             document.getElementById("taskInput").focus(); //* ustawia focus na inpucie po naciśnięciu buttona
 
             if (newTaskContent === "") {
-                
+
                 return;
             }
 
